be9f1984b933507dc90ceb125f38fcd14f3a67a4
Jorge Kenneth Nuezca <nuezcajorge@gmail.com>
2022-08-22 22:58:17 +0400

    Update list-filtering.js

--- list-filtering.js
+++ list-filtering.js
@@ -1,12 +1,12 @@
 function filter_list(list) {
     //Return a new array with the strings filtered out
     //return list.filter((element) => typeof element === "number");
-        var integers = [],
-            i = 0;
-        for (var i; i < list.length; i++) {
-            if (Number.isInteger(list[i])) {
-                integers.push(list[i]);
-            }
+    var integers = [],
+        i = 0;
+    for (var i; i < list.length; i++) {
+        if (Number.isInteger(list[i])) {
+            integers.push(list[i]);
         }
-        return integers;
+    }
+    return integers;
 }
