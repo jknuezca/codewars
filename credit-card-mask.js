30d8aac7df5efa6de1e6962ac1509b5aef6a3583
Jorge Kenneth Nuezca <nuezcajorge@gmail.com>
2022-08-26 00:43:57 +0400

    Update credit-card-mask.js

--- credit-card-mask.js
+++ credit-card-mask.js
@@ -1,8 +1,8 @@
 /*
-Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. 
-However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
+    Usually when you buy something, you're asked whether your credit card number, phone number or answer to your most secret question is still correct. 
+    However, since someone could look over your shoulder, you don't want that shown on your screen. Instead, we mask it.
 
-Your task is to write a function maskify, which changes all but the last four characters into '#'.
+    Your task is to write a function maskify, which changes all but the last four characters into '#'.
 */
 
 
